caf11b90b37f71ca782b01cbe9ac1b4881c2128c
Yak! <yak_ex@mx.scn.tv>
2012-09-10 22:48:08 +0900

    Implement commands.

--- compile_server/compile_server.pl
+++ compile_server/compile_server.pl
@@ -12,9 +12,14 @@ use English;
 use YAML;
 use File::Temp;
 
-# TODO: compiler configuration
-# TODO: output capture
-# TODO: command/response implementation (compile/status/result)
+use constant {
+	REQUESTED => 1,
+	COMPILING => 2,
+	RUNNING => 3,
+	FINISHED => 4,
+};
+
+# TODO: FINISHED reaper
 
 my $conf = YAML::LoadFile('config.yaml');
 
@@ -29,6 +34,72 @@ sub make_cl
 my %status;
 my $id = 0;
 
+sub invoke
+{
+	my ($handle, $json) = @_;
+
+	my $curid = $id++;
+
+	$status{$curid}{status} = REQUESTED;
+	$handle->push_write(json => { id => $curid });
+
+	my $fh = File::Temp->new(UNLINK=>0,SUFFIX=>'.cpp');
+	print $fh $json->{source};
+	close $fh;
+	my $source = $fh->filename;
+	my $fho = File::Temp->new(UNLINK=>0,SUFFIX=>($json->{execute} eq 'true' ? '.exe' : '.o'));
+	close $fho;
+	my $out = $fho->filename;
+
+	$status{$curid}{status} = COMPILING;
+	if($json->{execute} eq 'true') {
+		run_cmd([make_cl($json->{type}, 'link', $source, $out)], '<', '/dev/null', '>', \$status{$curid}{compile}, '2>', \$status{$curid}{compile})->cb(sub {
+print "---compile begin---\n";
+print $status{$curid}{compile};
+print "---compile  end ---\n";
+			$status{$curid}{status} = RUNNING;
+			run_cmd([$out], '<', '/dev/null', '>', \$status{$curid}{execute}, '2>', \$status{$curid}{execute})->cb(sub{
+print "---execute begin---\n";
+print $status{$curid}{execute};
+print "---execute  end ---\n";
+				unlink $out;
+				unlink $source;
+				$status{$curid}{status} = FINISHED;
+			});
+		});
+	} else {
+		run_cmd([make_cl($json->{type}, 'compile', $source, $out)], '<', '/dev/null', '>', \$status{$curid}{compile}, '2>', \$status{$curid}{compile})->cb(sub {
+print "---compile begin---\n";
+print $status{$curid}{compile};
+print "---compile  end ---\n";
+			unlink $out;
+			unlink $source;
+			$status{$curid}{status} = FINISHED;
+		});
+	}
+}
+
+sub status
+{
+	my ($handle, $json) = @_;
+
+# TODO: check unknown ID
+	$handle->push_write(json => { id => $json->{id}, status => $status{$json->{id}}{status} });
+}
+
+sub result
+{
+	my ($handle, $json) = @_;
+
+	$handle->push_write(json => { id => $json->{id}, execute => $status{$json->{id}}{execute}, compile => $status{$json->{id}}{compile} });
+}
+
+my %handler = (
+	invoke => \&invoke,
+	status => \&status,
+	result => \&result,
+);
+
 tcp_server '127.0.0.1', 8888, sub {
 	my ($fh, $host, $port) = @_;
 
@@ -42,39 +113,8 @@ tcp_server '127.0.0.1', 8888, sub {
 	my @handler; @handler = (json => sub {
 		print "handler\n";
 		my ($handle, $json) = @_;
-		my $curid = $id++;
-
-		my $fh = File::Temp->new(UNLINK=>0,SUFFIX=>'.cpp');
-		print $fh $json->{source};
-		close $fh;
-		my $source = $fh->filename;
-		my $fho = File::Temp->new(UNLINK=>0,SUFFIX=>($json->{execute} eq 'true' ? '.exe' : '.o'));
-		close $fho;
-		my $out = $fho->filename;
-
-		if($json->{execute} eq 'true') {
-			run_cmd([make_cl($json->{type}, 'link', $source, $out)], '<', '/dev/null', '>', \$status{$curid}{compile}, '2>', \$status{$curid}{compile})->cb(sub {
-print "---compile begin---\n";
-print $status{$curid}{compile};
-print "---compile  end ---\n";
-				run_cmd([$out], '<', '/dev/null', '>', \$status{$curid}{execute}, '2>', \$status{$curid}{execute})->cb(sub{
-print "---execute begin---\n";
-print $status{$curid}{execute};
-print "---execute  end ---\n";
-					unlink $out;
-					unlink $source;
-				});
-			});
-		} else {
-			run_cmd([make_cl($json->{type}, 'compile', $source, $out)], '<', '/dev/null', '>', \$status{$curid}{compile}, '2>', \$status{$curid}{compile})->cb(sub {
-print "---compile begin---\n";
-print $status{$curid}{compile};
-print "---compile  end ---\n";
-				unlink $out;
-				unlink $source;
-			});
-		}
-
+# TODO: check unknown command
+		$handler{$json->{command}}->($handle, $json);
 		$handle->push_read(@handler);
 	});
 	$handle->push_read(@handler);
--- compile_server/test_client.pl
+++ compile_server/test_client.pl
@@ -5,6 +5,8 @@ use warnings;
 use AnyEvent;
 use AnyEvent::Handle;
 
+use Data::Dumper;
+
 # TODO: command line option handling
 # TODO: type should be extracted from configuration
 
@@ -12,15 +14,17 @@ my $cv = AnyEvent->condvar;
 
 my $handle = AnyEvent::Handle->new(
 	connect => ['127.0.0.1', 8888],
-	on_drain => sub { $cv->send },
+#	on_drain => sub { $cv->send },
 );
 
-$handle->push_write(json => {type=>'gcc45',execute=>'true',source=><<'EOF'});
+$handle->push_write(json => {command=>'invoke',type=>'gcc45',execute=>'true',source=><<'EOF'});
 #include <iostream>
+#include <unistd.h>
 
 int main(void)
 {
 	int n;
+	sleep(5);
 	std::cout << "Run: " << __GNUC__ << '.' << __GNUC_MINOR__ << '.' << __GNUC_PATCHLEVEL__ << std::endl;
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
 	std::cout << "     with C++0X mode" << std::endl;
@@ -28,5 +32,30 @@ int main(void)
 	return 0;
 }
 EOF
+$handle->push_read(json => sub {
+	my ($handle, $json) = @_;
+	print "ID $json->{id}\n";
+	$handle->push_write(json => {command=>'status',id=>$json->{id}});
+	my @handler; @handler = (
+		json => sub {
+			my ($handle, $json) = @_;
+			if($json->{status} == 4) {
+				$handle->push_write(json => {command=>'result',id=>$json->{id}});
+				$handle->push_read(json => sub {
+					my ($handle, $json) = @_;
+					print Data::Dumper->Dump([$json]);
+					$cv->send;
+				});
+			} else {
+				# TODO: async
+				print "ID $json->{id} $json->{status}\n";
+				sleep 1;
+				$handle->push_write(json => {command=>'status',id=>$json->{id}});
+				$handle->push_read(@handler);
+			}
+		}
+	);
+	$handle->push_read(@handler);
+});
 
 $cv->recv;
