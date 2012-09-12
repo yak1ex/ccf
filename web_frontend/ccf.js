4f2d6574fea2218d68bc068e502353dae0e46663
Yak! <yak_ex@mx.scn.tv>
2012-09-13 00:52:57 +0900

    Add result tab updater.

--- web_frontend/ccf.js
+++ web_frontend/ccf.js
@@ -1,5 +1,7 @@
 $(function() {
-    var idmap;
+	var status = {};
+	var idxmap = {};
+	var idmap = {};
 	// Init
 	// TODO: Select all and deselect all
 	$.ajax({
@@ -26,6 +28,22 @@ $(function() {
 		$('#source-label').addClass('ui-widget-header ui-corner-all');
 		$('#source').addClass('ui-corner-all');
 	});
+	// Status updater
+	var updater = function() {
+		var idx = $('#result').tabs('option', 'selected');
+		if(idx != 0 && status[idxmap[idx]].status != 4) {
+			console.log('IDX:' + idx + 'IDXMAP:' + idxmap[idx] + 'ID:' + status[idxmap[idx]].id);
+			$.ajax({
+				url: 'ccf.cgi',
+				data: { command: 'status', id: status[idxmap[idx]].id },
+				dataType: 'json'
+			}).done(function(msg) {
+				status[idmap[msg.id]].status = msg.status;
+			});
+			$('#result').tabs('load', idx);
+			setTimeout(updater, 1000);
+		}
+	};
 	// Invoke
 	// TODO: actual implementation
 	$('#form').submit(function() {
@@ -39,11 +57,19 @@ $(function() {
                      + '&execute=' + ($('#compile:checked').length == 0 ? 'true' : 'false'),
                 dataType: 'json'
 			}).done(function(msg) {
-				idmap[obj.name] = msg.id;
+				status[obj.name].id = msg.id;
+				idmap[msg.id] = obj.name;
+				$('#result').tabs('url', status[obj.name].idx, 'ccf.cgi?command=status&id=' + msg.id);
 			});
+			$('#result').tabs('add', 'ccf.dummy.html', obj.name);
+			status[obj.name] = { status: 0, idx: $('#result').tabs('length')-1 };
+			idxmap[$('#result').tabs('length')-1] = obj.name;
 		});
-		$('#result').tabs('add', 'ccf.dummy.html', obj.name);
 		return false;
 	});
 	$('#result').tabs();
+	$('#result').bind("tabsselect", function(event, ui) {
+		setTimeout(updater, 1000);
+		return true;
+	});
 });
