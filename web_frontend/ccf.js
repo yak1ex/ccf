d52451002e09b2aacae9a9f8ab30095d1f1c90b7
Yak! <yak_ex@mx.scn.tv>
2012-09-12 13:22:55 +0900

    Apply jQuery theme for whole page.

--- web_frontend/ccf.js
+++ web_frontend/ccf.js
@@ -6,20 +6,24 @@ $(function() {
 		data: { command: 'list' },
 		dataType: 'json'
 	}).done(function(msg) {
+        // Setup compiler types
 		$.each(msg, function(key, value) {
 			$('#compiler_types').append('<input type="checkbox" class="ctypes" id="' + key + '" name="' + key + '"><label for="' + key + '">[' + key + '] ' + value + '</label><br>');
 		});
-		$('#form').addClass('ui-widget');
-		$('label').addClass('ui-widget-header');
-		$('textarea').addClass('ui-widget-content');
-		$('.ctypes').change(function(e) {
-			$('.ctypes').removeClass('ui-status-active');
-			$.each($('.ctypes:checked'), function(idx, obj) {
-				window.alert(obj.id);
-				obj.addClass('ui-status-active');
-				$('label[for="'+obj.id+'"]').addClass('ui-status-active');
-			});
+        // Apply jQuery theme and add icons
+		$('input:checkbox').button({ icons: { primary: 'ui-icon-minus' } })
+        .change(function(e) {
+			if($(this).button('option', 'icons').primary == 'ui-icon-minus') {
+				$(this).button('option', 'icons', { primary: 'ui-icon-plus' });
+			} else {
+				$(this).button('option', 'icons', { primary: 'ui-icon-minus' });
+			}
 		});
+        // Apply jQuery theme
+        // TODO: Use table?
+		$('#input-container').addClass('ui-widget ui-widget-content ui-corner-all');
+		$('#source-label').addClass('ui-widget-header ui-corner-all');
+		$('#source').addClass('ui-corner-all');
 	});
 	// Invoke
 	// TODO: actual implementation
