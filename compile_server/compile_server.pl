637ecfb9ba62667b1c93a0088f0f6c6490ccafd6
Yak! <yak_ex@mx.scn.tv>
2012-09-16 00:38:48 +0900

    Add command line option handling (close #5).

--- compile_server/compile_server.pl
+++ compile_server/compile_server.pl
@@ -11,6 +11,8 @@ use AnyEvent::Util; # run_cmd
 use English;
 use YAML;
 use File::Temp;
+use Getopt::Std;
+use Pod::Usage;
 
 BEGIN {
 	if($^O eq 'cygwin') {
@@ -26,12 +28,27 @@ use constant {
 	FINISHED => 4,
 };
 
+our ($VERSION) = '0.01';
+
 # TODO: clear command
 # TODO: FINISHED reaper
 # TODO: Optional log
 # TODO: Using sandbox
 
-my $conf = YAML::LoadFile('config.yaml');
+#
+# Init
+#
+
+my %opts;
+$Getopt::Std::STANDARD_HELP_VERSION = 1;
+getopts('hp:c:k:v', \%opts);
+pod2usage(-verbose => 1) if exists $opts{h};
+my $confname = $opts{c} // 'config.yaml';
+! -f $confname and pod2usage(-msg => "\n$0: ERROR: Configuration file `$confname' does not exist.\n", -exitval => 1, -verbose => 0);
+my $conf = YAML::LoadFile($confname);
+my $confkey = $opts{k} // $OSNAME;
+! exists $conf->{$confkey} and pod2usage(-msg => "\n$0: ERROR: Configuration key `$confkey' does not exist in configuration file.\n", -exitval => 1, -verbose => 0);
+my $port = $opts{p} // 8888;
 
 # TODO: Separate cygwin2native handling to module?
 
@@ -75,7 +92,7 @@ sub make_arg
 # TODO: error check
 #	return map { $_ eq '$input' ? $input : $_ eq '$output' ?  $output : $_ } @{$conf->{$OSNAME}{$type}{$mode}};
 	my @res = map { my $t = $_; $t =~ s/\$input/$input/; $t =~ s/\$output/$output/; $t; } @{$conf->{$OSNAME}{$type}{$mode}}; 
-	print join(' ', @res), "\n";
+	$opts{v} and print STDERR join(' ', @res), "\n";
 	return ([@res], '<', '/dev/null', '>', $capture, '2>', $capture, @arg);
 }
 
@@ -116,15 +133,11 @@ sub invoke
 	if($json->{execute} eq 'true') {
 		run_cmd(make_arg($json->{type}, 'link', $source, $out, \$status{$curid}{compile}))->cb(sub {
 			$status{$curid}{compile} = dec($json->{type}, $status{$curid}{compile});
-print "---compile begin---\n";
-print $status{$curid}{compile};
-print "---compile  end ---\n";
+			$opts{v} and print STDERR "---compile begin---\n$status{$curid}{compile}---compile  end ---\n";
 			$status{$curid}{status} = RUNNING;
 			chmod 0711, $out if is_cygwin2native($json->{type});
 			run_cmd([$out], '<', '/dev/null', '>', \$status{$curid}{execute}, '2>', \$status{$curid}{execute})->cb(sub{
-print "---execute begin---\n";
-print $status{$curid}{execute};
-print "---execute  end ---\n";
+				$opts{v} and print STDERR "---execute begin---\n$status{$curid}{execute}---execute  end ---\n";
 				unlink $out;
 				unlink $source;
 				$status{$curid}{status} = FINISHED;
@@ -134,9 +147,7 @@ print "---execute  end ---\n";
 		my $env = setenv($json->{type});
 		run_cmd(make_arg($json->{type}, 'compile', $source, $out, \$status{$curid}{compile}))->cb(sub {
 			$status{$curid}{compile} = dec($json->{type}, $status{$curid}{compile});
-print "---compile begin---\n";
-print $status{$curid}{compile};
-print "---compile  end ---\n";
+			$opts{v} and print STDERR "---compile begin---\n$status{$curid}{compile}---compile  end ---\n";
 			unlink $out;
 			unlink $source;
 			$status{$curid}{status} = FINISHED;
@@ -177,10 +188,11 @@ my %handler = (
 	list => \&list,
 );
 
-tcp_server '127.0.0.1', 8888, sub {
+$opts{v} and print STDERR "listening on port $port...\n";
+tcp_server '127.0.0.1', $port, sub {
 	my ($fh, $host, $port) = @_;
 
-	print "connect\n";
+	$opts{v} and print STDERR "connect\n";
 
 	my $handle; $handle = AnyEvent::Handle->new(
 		fh => $fh,
@@ -188,9 +200,9 @@ tcp_server '127.0.0.1', 8888, sub {
 		on_error => sub { $handle->destroy; },
 	);
 	my @handler; @handler = (json => sub {
-		print "handler\n";
 		my ($handle, $json) = @_;
 		if(exists $json->{command} && exists $handler{$json->{command}}) {
+			$opts{v} and print STDERR "handler called by command `$json->{command}'.\n";
 			$handler{$json->{command}}->($handle, $json);
 		} else {
 			my $command = '';
@@ -205,3 +217,63 @@ tcp_server '127.0.0.1', 8888, sub {
 };
 
 AnyEvent->condvar->recv;
+
+__END__
+
+=head1 NAME
+
+compile_server.pl - Compile server for C++ Compiler Farm
+
+=head1 SYNOPSIS
+
+compile_server [-h] [-p I<port>] [-c I<filename>] [-k I<key>] [-v]
+
+  # show help (this POD) and exit
+  compile_server.pl -h
+  
+  # read configuration from config.yaml and $OSNAME is used for configuration key
+  compile_server.pl
+  
+  # read configuration from conf.yaml and cygwin-test is used for configuration key
+  compile_server.pl -c conf.yaml -k cygwin-test
+
+  # listen on port 8880 with logging
+  compile_server.pl -p 8880 -v
+
+=head1 DESCRIPTION
+
+compile_server.pl is a backend compile server for C++ Compiler Farm.
+It accepts source code and return its compilation / execution results.
+
+=head1 OPTIONS
+
+=over 4
+
+=item -h
+
+Show this POD and exit
+
+=item -p I<port>
+
+Listen port. Defaults to 8888.
+
+=item -c I<filename>
+
+Configuration YAML file name. Defaults to config.yaml.
+
+=item -k I<key>
+
+Configuration key. The key must exist in the configuration file.
+Defaults to $OSNAME.
+
+=item -v
+
+Enable verbose logging.
+
+=back
+
+=head1 AUTHOR
+
+Yak! <yak_ex@mx.scn.tv>
+
+=cut
