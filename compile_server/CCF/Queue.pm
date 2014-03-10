0a5da42874cc46653791964bbe8798f5e39fc3dd
Yak! <yak_ex@mx.scn.tv>
2014-03-10 12:49:45 +0900

    Rename internal methods.

--- compile_server/CCF/Queue.pm
+++ compile_server/CCF/Queue.pm
@@ -14,27 +14,29 @@ sub new
 	}, $class;
 }
 
-sub _call
+# precondition: $self->{_AVAIL} MUST be positive
+sub _enter
 {
 	my ($self, $coderef) = @_;
 	--$self->{_AVAIL};
 	my $ret = $coderef->();
 	if(eval { $ret->isa('AnyEvent::CondVar') }) {
 		$ret->cb(sub {
-			++$self->{_AVAIL};
-			$self->_take();
+			$self->_leave();
 		});
 	} else {
 		croak "a queued task does not return AnyEvent::CondVar";
 	}
 }
 
-sub _take
+sub _leave
 {
 	my ($self) = @_;
+	++$self->{_AVAIL};
 	if(@{$self->{_QUEUE}}) {
 		my $coderef = shift @{$self->{_QUEUE}};
-		$self->_call($coderef);
+		# NOTE: $self->{_AVAIL} should be positive
+		$self->_enter($coderef);
 	}
 }
 
@@ -42,7 +44,7 @@ sub enque
 {
 	my ($self, $coderef) = @_;
 	if($self->{_AVAIL} > 0) {
-		$self->_call($coderef);
+		$self->_enter($coderef);
 	} else {
 		push @{$self->{_QUEUE}}, $coderef;
 	}
